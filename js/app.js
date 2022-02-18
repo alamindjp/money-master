c7dcf1ec4a7d846be9234a26c0e988fa5043ea9a
alamindjp <alaminhossaindjp@gmail.com>
2022-02-18 21:29:35 +0600

    update js

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     }
     // // get food input
     const foodInput = getInputValue('food-expenses-input');
-    if (isNaN(foodInput) || foodInput <= 0) {
+    if (isNaN(foodInput) || foodInput < 0) {
         return alert('Please Check Food Amount')
     }
     else {
@@ -48,7 +48,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
     // // get rent input
     const rentInput = getInputValue('rent-expenses-input');
-    if (isNaN(rentInput) || rentInput <= 0) {
+    if (isNaN(rentInput) || rentInput < 0) {
         return alert('Please Check Rent Amount')
     }
     else {
@@ -57,7 +57,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
     // // get clothes input
     const clothesInput = getInputValue('clothes-expenses-input');
-    if (isNaN(clothesInput) || clothesInput <= 0) {
+    if (isNaN(clothesInput) || clothesInput < 0) {
         return alert('Please Check Rent Amount')
     }
     else {
@@ -79,3 +79,32 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 });
 
 // calculate savings 
+document.getElementById('savings-btn').addEventListener('click', function () {
+    // debugger;
+    const savingsfield = document.getElementById('saveings-persrnts');
+    const saveingsInput = parseInt(savingsfield.value);
+    const savingsPresent = saveingsInput / 100;
+    // get income Input
+    const incomeInputField = document.getElementById('total-income');
+    const inputAmount = parseFloat(incomeInputField.value);
+
+    const totalSavingsAmount = inputAmount * savingsPresent;
+
+    const netBalanceInputField = document.getElementById('net-balance');
+    const netBalance = parseFloat(netBalanceInputField.innerText);
+
+    // get savings Input and calculate amount
+
+    const savingsAmountField = document.getElementById('saving-amount');
+    if (isNaN(saveingsInput) || saveingsInput <= 0 || totalSavingsAmount > netBalance) {
+        return alert('Please Check Your Saving Percentage')
+    }
+    else {
+        savingsAmountField.innerText = totalSavingsAmount;
+    }
+    const saveingsAmount = parseFloat(savingsAmountField.innerText);
+    const remainingBalance = document.getElementById('remaining-balance');
+    const totalRemainingBalance = netBalance - saveingsAmount;
+    remainingBalance.innerText = totalRemainingBalance;
+
+});
